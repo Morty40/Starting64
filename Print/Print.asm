493f8b5b3fc2e0bc9b344e4295624dc4d59e2d21
Morty40 <morten@perriard.dk>
2026-07-18 21:21:56 +0200

    cleanup

--- Print/Print.asm
+++ Print/Print.asm
@@ -1,15 +1,16 @@
+; Print.asm
+; Use basic rom to print to screen
+; (c) Morten Perriartd 2026
 
 			; BASIC start
 			@basicStart()
-			@basicSys(0, start)
+			@basicSys(2026, start)
 			@basicEnd()
 start:
-			; print text 
-			lda #lo(text)
-			ldy #hi(text)
+			; text pointer in a=lo, y=hi
+			@lday(text)
 			jsr BASIC_ROM_STROUT
-			
 			rts
-text:
-			.text "HELLO WORLD"
+
+text:		.text "HELLO WORLD"
 			.byte 0
